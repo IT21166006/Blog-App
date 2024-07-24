821f2620b39b357ea01218731be85cd203d1e19e
Tdmaxer <49441022+Tdmaxer@users.noreply.github.com>
2024-07-24 20:55:39 +0530

    dotenv configuring

--- server.js
+++ server.js
@@ -4,5 +4,29 @@ const morgan = require("morgan");
 const colors = require("colors");
 const dotenv = require("dotenv");
 
+//env config
+dotenv.config();
+
 //rest object
 const app = express();
+
+//middelwares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+//routes
+app.use("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Welcome to blog app",
+    });
+});
+
+
+//listen
+app.listen(8080, () => {
+  console.log(
+    `Server Running on port 8080 `.bgCyan.white);
+  
+});
