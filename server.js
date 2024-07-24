f718f8df0b47552eba91947d015f8693cdaa5d6b
Tdmaxer <49441022+Tdmaxer@users.noreply.github.com>
2024-07-24 20:43:34 +0530

    project created work on server .js

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const colors = require("colors");
+const dotenv = require("dotenv");
+
+//rest object
+const app = express();
